497933b2f690ca2022e1cee5449041909b7cd958
John P Quense <jpquense@gmail.com>
2018-02-04 12:06:31 -0500

    connected api response to rendering function

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
-function normalizeData(data) {
-	// Data manipulation
-  const normalizedData = data;
-  return normalizedData;
-}
+"use strict";
 
 function renderDom(data) {
-	// Redenring methods
-  const html = data.map(item => `
+	// Rendering methods 
+  const articles = data[0];
+  const movies = data[1];
+  const videos = data[2];
+  const books = data[3];
+
+  console.log('renderDom ran')
+
+  const articlesHtml = articles.map(item => `
   	<div>
-    	<h2>${item.title}</h2>
-      <p>${item.desctiption}</p>
+   <h2>${item.title}</h2>
+   <p>${item.desctiption}</p>
+   </div>
+   `);
+  const moviesHtml = articles.map(item => `
+    <div>
+    <h2>${item.title}</h2>
+    <p>${item.desctiption}</p>
     </div>
-  `);
-  $('#app').html(html);
+    `);
+  const videosHtml = articles.map(item => `
+    <div>
+    <h2>${item.title}</h2>
+    <p>${item.desctiption}</p>
+    </div>
+    `);
+  const booksHtml = articles.map(item => `
+    <div>
+    <h2>${item.title}</h2>
+    <p>${item.desctiption}</p>
+    </div>
+    `);
+
+  $('.js-articles').html(articlesHtml);
+  $('.js-movies').html(moviesHtml);
+  $('.js-videos').html(videosHtml);
+  $('.js-books').html(booksHtml);
+
 }
 
 function handleApisResponse(responses) {
 	// Manipulte data and update them in desired way
-  const data = normalizeData(responses);
+  const data = responses;
   // Call renderind methods
-  renderReponses(data);
+  console.log(responses, data);
+  renderDom(data);
   // Hide temporary loader
-	  $('#loader').show();
+  $('.js-loader').removeClass('loader');
 }
 
 function searchApisFor(searchTerm) {
-	const articleCall = $.ajax({
+  console.log(`searchApisFor function was called for "${searchTerm}"`);
+  const articleCall = $.ajax({
     method: 'get',
     data: {
       'api-key': '3710cd87703f4791a369cf34c5139e41',
-    	q: searchTerm,
+      q: searchTerm,
     },
     url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
   });
 
   const movieCall = $.ajax({
-    method: 'get',
-    data: {
-      r: json,
-      plot: short
-    }, 
+    method: 'get', 
     url: `http://www.omdbapi.com/?t=${searchTerm}&apikey=2fd347a7`,
 
   });
@@ -55,7 +79,7 @@ function searchApisFor(searchTerm) {
     url: 'https://www.googleapis.com/youtube/v3/search',
   });
 
-    const bookCall = $.ajax({
+  const bookCall = $.ajax({
     method: 'get',
     data: {
       key: 'AIzaSyDGFCGIZUTEwOQ3YCbfdk1kn4ug1ZWXC48',
@@ -71,9 +95,13 @@ function searchApisFor(searchTerm) {
   Promise.all([articleCall, movieCall, videoCall, bookCall]).then(handleApisResponse);
 }
 
-$('.js-search').click(function(e) {
-	e.preventDefault();
-  const searchValue = $('.search-input').val();
-  searchApisFor(searchValue);
-  $('#loader').show();
-})
+$('.js-form').submit(function(e) {
+  console.log('event handler has been called');
+  e.preventDefault();
+  const searchTarget = $(event.currentTarget).find('#js-input');
+  const searchValue = searchTarget.val();
+    // clear input
+    searchTarget.val("");
+    searchApisFor(searchValue);
+    $('.js-loader').addClass('loader');
+  })
